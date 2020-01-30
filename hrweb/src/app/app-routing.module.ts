2a38e9e716dff7a1e306c702c15daa38c0c4d577
hw5711 <huanwu5711@gmail.com>
2020-01-30 17:40:20 -0600

    link form page

--- hrweb/src/app/app-routing.module.ts
+++ hrweb/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-  { path: "home", component: HomeComponent }
+  { path: "home", component: HomeComponent },
+  { path: "form", component: FormComponent }
 ];
 
 @NgModule({
