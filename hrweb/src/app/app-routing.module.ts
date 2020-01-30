b9dad086af507cffef06afbaffa34216dbe04a9b
hw5711 <huanwu5711@gmail.com>
2020-01-29 20:31:34 -0600

    home, login and register page html uploaded

--- hrweb/src/app/app-routing.module.ts
+++ hrweb/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "home", component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
