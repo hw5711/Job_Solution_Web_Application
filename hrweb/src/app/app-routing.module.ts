378e46182596dcfb12448d445f1a8b9e539102ab
SharmilaSrinivasa <40612676+SharmilaSrinivasa@users.noreply.github.com>
2020-02-06 15:42:36 -0600

    update

--- hrweb/src/app/app-routing.module.ts
+++ hrweb/src/app/app-routing.module.ts
@@ -3,16 +3,27 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { RetriveComponent } from './retrive/retrive.component';
+import { ResetpasswordComponent } from './retrive/resetpassword/resetpassword.component';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
 import { WorkformComponent } from './workform/workform.component';
 
+import { LoginGuard } from "./login/login.guard";
+
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
+<<<<<<< HEAD
   { path: "home", component: HomeComponent },
   { path: "form", component: FormComponent },
   { path: "workform", component: WorkformComponent }
+=======
+  { path: "", component: HomeComponent },
+  { path: "form", component: FormComponent },
+  { path: "retrive", component: RetriveComponent },
+  { path: "reset", component: ResetpasswordComponent }
+>>>>>>> 2e72500607feefde1e367fd2985c7ec0a19b1fe4
 ];
 
 @NgModule({
--- hrweb/src/app/app.module.ts
+++ hrweb/src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +15,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
+<<<<<<< HEAD
 import { WorkformComponent } from './workform/workform.component';
+=======
+import { RetriveComponent } from './retrive/retrive.component';
+import { ResetpasswordComponent } from './retrive/resetpassword/resetpassword.component';
+
+>>>>>>> 2e72500607feefde1e367fd2985c7ec0a19b1fe4
 
 @NgModule({
   declarations: [
@@ -16,11 +30,22 @@ import { WorkformComponent } from './workform/workform.component';
     RegisterComponent,
     HomeComponent,
     FormComponent,
+<<<<<<< HEAD
     WorkformComponent
+=======
+    RetriveComponent,
+    ResetpasswordComponent
+>>>>>>> 2e72500607feefde1e367fd2985c7ec0a19b1fe4
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
