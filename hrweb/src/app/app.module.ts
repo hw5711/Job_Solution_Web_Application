6f1a9629cf9fce2aa12712feb157d96d0b760d6d
SharmilaSrinivasa <40612676+SharmilaSrinivasa@users.noreply.github.com>
2020-02-06 17:54:29 -0600

    update

--- hrweb/src/app/app.module.ts
+++ hrweb/src/app/app.module.ts
@@ -7,20 +7,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ToastrModule } from 'ngx-toastr';
 import { MatDialogModule } from '@angular/material';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
-<<<<<<< HEAD
 import { WorkformComponent } from './workform/workform.component';
-=======
 import { RetriveComponent } from './retrive/retrive.component';
 import { ResetpasswordComponent } from './retrive/resetpassword/resetpassword.component';
-
->>>>>>> 2e72500607feefde1e367fd2985c7ec0a19b1fe4
 
 @NgModule({
   declarations: [
@@ -29,12 +24,9 @@ import { ResetpasswordComponent } from './retrive/resetpassword/resetpassword.co
     RegisterComponent,
     HomeComponent,
     FormComponent,
-<<<<<<< HEAD
-    WorkformComponent
-=======
+    WorkformComponent,
     RetriveComponent,
     ResetpasswordComponent
->>>>>>> 2e72500607feefde1e367fd2985c7ec0a19b1fe4
   ],
   imports: [
     BrowserModule,
